53190f3e598016ee074879a8e093149a322e5407
Luis Ramirez <ramirezluis1598@gmail.com>
2023-04-27 18:25:03 -0400

    Ajustes sobre los factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        \App\Models\Note::factory(30)->create();
 
         \App\Models\User::factory()->create([
             'name'  => 'admin',
             'email' => 'admin@mail.com',
             'password' => bcrypt('123456')
         ]);
+
+        \App\Models\Team::factory()->create([
+            'name' => 'admin'.'\'s Team',
+            'user_id' => 1,
+            'personal_team' => true,
+        ]);
+
+        // \App\Models\Team::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        \App\Models\Note::factory(30)->create();
     }
 }
